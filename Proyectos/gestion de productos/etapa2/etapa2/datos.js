e4682318d703c2ae417eca71c61c170e9c435465
luciopompei <pompeilucio@gmail.com>
2024-08-28 17:18:26 -0300

    etapa 2 terminada

---
+++ Proyectos/gestion de productos/etapa2/etapa2/datos.js
@@ -0,0 +1,8 @@
+let productos = [
+    {id:1,nombre:"shanpu",precio: 100,cantidad: 5},
+    {id:2,nombre:"javon",precio:500,cantidad: 3},
+    {id:3,nombre:"cepillo",precio: 400,cantidad: 2},
+    {id:4,nombre:"tenedor",precio: 300,cantidad: 7},
+    {id:5,nombre:"inodoro",precio: 200,cantidad: 9}
+                ]
+                export default productos;
